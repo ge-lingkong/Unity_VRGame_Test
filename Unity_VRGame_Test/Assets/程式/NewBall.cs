746f50a3169870423a27f1e3dab6b94e22bf2c78
ge-lingkong <09160031@me.mcu.edu.tw>
2023-03-10 21:48:59 +0800

    場景、傳送區、球、按鈕

---
+++ Unity_VRGame_Test/Assets/程式/NewBall.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class NewBall : MonoBehaviour
+{
+    [Header("球球")]
+    public GameObject ball;
+    [Header("生成點")]
+    public Transform point;
+    public void newBall()
+    {
+        Instantiate(ball, point.position, point.rotation,point);
+    }
+}
